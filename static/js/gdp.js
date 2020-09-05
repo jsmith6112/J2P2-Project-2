59a73053137dc0269df6f77ef7e38df5f2a3ae5b
Pariya P <pariya.poocharoen@campus.viu.edu>
2020-09-05 07:47:00 -0400

    first upload to pariya's branch

--- static/js/gdp.js
+++ static/js/gdp.js
@@ -1,44 +1,100 @@
-function stGDP() {
+function init() {
 
-  var url = "/states_gdp";
+  var url = "/gdp";
 
   d3.json(url).then(function(res) {
 
-    var key = Object.keys(res);
-    var value = Object.values(res).slice(0,51);
-    var st = (res.map(d => d.States)).slice(0,51);
+    var year = res.map(d => d.Year);
+    var st_gdp = res.map(d => d.GDP_millions);
+    var st = res.map(d => d.STATE);
 
+    var trace = [{
+      'x': year,
+      'y': st_gdp,
+      'type': 'bar',
+      'marker': {
+        color: '#B592A0'
+      }
+    }];
 
-    console.log(key[0]);
-    console.log(value);
-    console.log(value[0]);
-    console.log('This is 2012 GDP ' + '$' + value[0]['2012'] + ' of ' + st[0]);
-    console.log(st);
+    var layout = {
+      title: "Gross Domestic Products (GDP) Overtime",
+      xaxis: {
+        title: "Year"
+      },
+      yaxis: {
+        title: "GDP (millions)"
+      }
+    };
 
+    var config = {responsive: true}
 
-    // var gdpData = [];
-    // for (i=0; i < value.length; i++) {
-    //   var g =
-    // }
-
-    // var trace = [{
-    //   'x': st,
-    //   'y': y2012,
-    //   'type': 'line'
-    // }];
-    //
-    // var layout = {
-    //   title: "Pet GDP Per Capita By States",
-    //   xaxis: {
-    //     title: "States"
-    //   },
-    //   yaxis: {
-    //     title: "GDP Per Capita"
-    //   }
-    // };
-    //
-    // Plotly.newPlot("st_gdp", trace, layout);
+    Plotly.newPlot("plot_gdp", trace, layout, config);
   });
 }
+init();
 
-stGDP();
+
+// On change function when select the dropdown filter
+d3.select('#st_selector').on('change', updateState);
+
+function updateState() {
+
+  selectedState = d3.select('#st_selector').node().value
+
+  console.log(`selected state is ${selectedState}`);
+
+  var url = "/gdp";
+  d3.json(url).then(function(res) {
+
+    // Use filter() to filter yearly data
+    var filtered_data = res.filter(d => d.STATE == selectedState);
+
+    var year = filtered_data.map(d => d.Year);
+    var st_gdp = filtered_data.map(d => d.GDP_millions);
+
+    // var counts_sorted = counts.sort(function(a,b){return b-a});
+    // var counts_top10 = counts_sorted.slice(0,10);
+
+    // console.log(`TOP 10 FOR ${selectedState} ARE ${counts_top10}`);
+    // console.log(industry.slice(0,10));
+    // console.log(counts_top10);
+
+    var trace = [{
+      'x': year,
+      'y': st_gdp,
+      type: 'bar',
+      // orientation: 'h',
+      marker:{
+        color: ['#738396',
+                '#4ECDC4',
+                '#BB7E8C',
+                '#FF6B6B',
+                '#FFE66D',
+                '#738396',
+                '#4ECDC4',
+                '#BB7E8C',
+                '#FF6B6B',
+                '#FFE66D']
+      },
+      text: year.map(String),
+      textposition: 'auto'
+
+    }];
+
+    var layout = {
+      title: "Gross Domestic Products (GDP) Overtime",
+      xaxis: {
+        title: "Year"
+      },
+      yaxis: {
+        title: "GDP (millions)",
+        showticklabels: false
+      }
+    };
+
+    var config = {responsive: true}
+
+    Plotly.react("plot_gdp", trace, layout, config);
+  });
+}
